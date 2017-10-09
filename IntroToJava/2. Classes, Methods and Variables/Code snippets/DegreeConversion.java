a5beaf8b3987821e12a08727b4e28fe416d022b0
Marc Cataford <c.marcandre@gmail.com>
2017-10-08 20:50:19 -0400

    Example snippet.

---
+++ IntroToJava/2. Classes, Methods and Variables/Code snippets/DegreeConversion.java
@@ -0,0 +1,19 @@
+/*
+*	DegreeConversion.java
+*
+*	This is an exercise aimed at producing a small program that computes
+*	the fahrenheit equivalent value to a given value in degrees celsius.
+*
+*	You should output your results as part of a sentence.
+*/
+
+public class DegreeConversion {
+	public static void main(String[] args) {
+
+		int celsius_degrees = 26; //Replace this value to test your program thoroughly.
+
+		/*
+		*	Write out your code here!
+		*/
+	}
+}
